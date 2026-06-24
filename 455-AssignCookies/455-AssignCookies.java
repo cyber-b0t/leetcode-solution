e751a38a974eb71aabc5fe0b02118da20a253c90
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-06-24 20:54:01 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,21 @@
+// Last updated: 6/24/2026, 8:54:01 PM
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+            cookie++;
+        }
+
+        return child;
+    }
+}
