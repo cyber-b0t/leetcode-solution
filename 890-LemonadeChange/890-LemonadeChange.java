6bdeaae337db85d09333b852ed7e2b214f28a82e
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-06-24 20:53:55 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,30 @@
+// Last updated: 6/24/2026, 8:53:55 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
