7199e9b339c198593d81a384f2e5864eb0b2248c
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-06-30 13:37:12 +0530

    File updated at 6/30/2026, 1:37:11 PM

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.java
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -1,45 +1,36 @@
-// Last updated: 6/30/2026, 1:36:44 PM
+// Last updated: 6/30/2026, 1:37:11 PM
 1class Solution {
 2    public List<List<Integer>> generate(int numRows) {
 3        List<List<Integer>> ans = new ArrayList<>();
 4
-5        if(numRows==1){
+5        for(int i=0;i<numRows;i++){
 6            List<Integer> arr = new ArrayList<>();
-7            arr.add(1);
-8            ans.add(arr);
-9            return ans;
-10        }
-11
-12        
-13
-14        for(int i=0;i<numRows;i++){
-15            List<Integer> arr = new ArrayList<>();
-16
-17            if(i==0){
-18                arr.add(1);
-19                ans.add(arr);
-20            }else if(i==1){
-21                arr.add(1);
-22                arr.add(1);
-23                ans.add(arr);
-24            }else{
-25                arr.add(1);
-26                int x=0,y=1;
-27                while(y<i){
-28                    int n = ans.get(i-1).get(x);
-29                    int m = ans.get(i-1).get(y);
-30
-31                    int s = n+m;
-32                    arr.add(s);
-33                    x++;
-34                    y++;
-35                }
-36
-37                arr.add(1);
-38                ans.add(arr);
-39            }          
-40        }
-41
-42        return ans;
-43    }
-44}
+7
+8            if(i==0){
+9                arr.add(1);
+10                ans.add(arr);
+11            }else if(i==1){
+12                arr.add(1);
+13                arr.add(1);
+14                ans.add(arr);
+15            }else{
+16                arr.add(1);
+17                int x=0,y=1;
+18                while(y<i){
+19                    int n = ans.get(i-1).get(x);
+20                    int m = ans.get(i-1).get(y);
+21
+22                    int s = n+m;
+23                    arr.add(s);
+24                    x++;
+25                    y++;
+26                }
+27
+28                arr.add(1);
+29                ans.add(arr);
+30            }          
+31        }
+32
+33        return ans;
+34    }
+35}
