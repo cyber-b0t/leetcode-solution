0a9e0fad3d72e59677558ec33fb8b44605a0f631
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-05 22:51:40 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,16 @@
+// Last updated: 7/5/2026, 10:51:39 PM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if (n <= 2) return n;
+4
+5        int[] dp = new int[n + 1];
+6        dp[1] = 1;                          
+7        dp[2] = 2;                         
+8
+9        for (int i = 3; i <= n; i++) {
+10            dp[i] = dp[i-1] + dp[i-2];      
+11        }
+12
+13        return dp[n];
+14    }
+15}
