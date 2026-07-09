2f3e5158203bf7e7671593425fc543144f0125fb
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-09 18:45:55 +0530

    File updated at 7/9/2026, 6:45:54 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -1,29 +1,24 @@
-// Last updated: 6/24/2026, 8:54:02 PM
-class Solution {
-    public int characterReplacement(String s, int k){
-        int[] freq = new int[26];
-        int L=0;
-        int R=0;
-
-        int maxFreq=0; 
-        int max=0;
-
-        while(R<s.length()){
-            char c = s.charAt(R);
-            freq[c-'A']++;
-            maxFreq = Math.max(maxFreq,freq[c-'A']);
-
-            int winSize = R-L+1;
-
-            if(winSize-maxFreq>k){
-                char x = s.charAt(L);
-                freq[x-'A']--;
-                L++;
-            }
-            max = Math.max(max,R-L+1);
-            R++;
-        }
-
-        return max;
-    }
-}
+// Last updated: 7/9/2026, 6:45:54 PM
+1class Solution {
+2    public int characterReplacement(String s, int k) {
+3        HashMap<Character,Integer> mpp = new HashMap<>();
+4        int maxFreq=0;
+5        int maxLen=0;
+6        int L=0;
+7        for(int R=0;R<s.length();R++){
+8            char c = s.charAt(R);
+9            mpp.put(c,mpp.getOrDefault(c,0)+1);
+10            maxFreq = Math.max(maxFreq,mpp.get(c));
+11            int currLen = R-L+1; 
+12            while(currLen-maxFreq>k){
+13                mpp.put(s.charAt(L),mpp.getOrDefault(s.charAt(L),0)-1);
+14                L++;
+15                currLen = R-L+1;
+16            }
+17
+18            maxLen = Math.max(maxLen,currLen);
+19        }
+20
+21        return maxLen;
+22    }
+23}
