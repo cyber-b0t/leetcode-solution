813e8cc5b9cd35f18c6cbb087ab6278d1c8cc094
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-10 20:05:49 +0530

    File updated at 7/10/2026, 8:05:49 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,22 +1,22 @@
-// Last updated: 6/24/2026, 8:54:39 PM
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-
-        int[] ans = new int[2];
-
-        for(int i=0;i<nums.length;i++){
-            int need = target-nums[i];
-
-            if(!mpp.containsKey(need)){
-                mpp.put(nums[i],i);
-            }else{
-                ans[0] = mpp.get(need);
-                ans[1] = i;
-                break;
-            }
-        }
-
-        return ans;
-    }
-}
+// Last updated: 7/10/2026, 8:05:49 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        HashMap<Integer,Integer> freq = new HashMap<>();
+4        int[] arr = new int[2];
+5
+6        for(int i=0;i<nums.length;i++){
+7            int n = nums[i];
+8            int need = target-n;
+9            
+10            if(freq.containsKey(need)){
+11                arr[0] = i;
+12                arr[1] = freq.get(need);
+13                break;
+14            }else{
+15                freq.put(n,i);
+16            }
+17        }
+18
+19        return arr;
+20    }
+21}
