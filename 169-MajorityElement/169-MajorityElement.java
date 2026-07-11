a00a87c8563076a4204125247a9c299f3fb553e6
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-11 18:59:58 +0530

    File updated at 7/11/2026, 6:59:58 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,21 +1,21 @@
-// Last updated: 6/24/2026, 8:54:09 PM
-class Solution {
-    public int majorityElement(int[] nums) {
-        int el=nums[0];
-        int count=0;
-
-        for(int i=0;i<nums.length;i++){
-            if(el==nums[i]){
-                count++;
-            }else{
-                count--;
-                if(count==0){
-                    el = nums[i];
-                    count++;
-                }
-            }
-        }
-
-        return el;
-    }
-}
+// Last updated: 7/11/2026, 6:59:58 PM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int el=nums[0];
+4        int cnt=1;
+5        
+6        for(int i=1;i<nums.length;i++){
+7            if(el==nums[i]){
+8                cnt++;
+9            }else{
+10                cnt--;
+11                if(cnt==0){
+12                    el=nums[i];
+13                    cnt=1;
+14                }
+15            }
+16        }
+17        
+18        return el;
+19    }
+20}
