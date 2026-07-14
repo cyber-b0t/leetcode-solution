35e53e2db5e80a9489a90227c971bedf4cc9edcb
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-14 16:26:38 +0530

    File updated at 7/14/2026, 4:26:38 PM

--- 191-Numberof1Bits/191-Numberof1Bits.java
+++ 191-Numberof1Bits/191-Numberof1Bits.java
@@ -1,10 +1,10 @@
-// Last updated: 7/14/2026, 4:24:58 PM
+// Last updated: 7/14/2026, 4:26:38 PM
 1class Solution {
 2    public int hammingWeight(int n) {
-3        int cnt=0;
-4        while(n!=0){
-5            if((n&1)==1) cnt++;
-6            n = n>>1;
+3        int cnt = 0;
+4        while (n != 0) {
+5            n &= (n - 1);
+6            cnt++;
 7        }
 8        return cnt;
 9    }
