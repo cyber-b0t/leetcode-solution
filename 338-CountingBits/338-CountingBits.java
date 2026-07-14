d2ed560f97c997607c7ba11cacde23bfef1bf5a3
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-14 16:45:33 +0530

    File updated at 7/14/2026, 4:45:33 PM

--- 338-CountingBits/338-CountingBits.java
+++ 338-CountingBits/338-CountingBits.java
@@ -1,20 +1,12 @@
-// Last updated: 7/14/2026, 4:42:36 PM
+// Last updated: 7/14/2026, 4:45:33 PM
 1class Solution {
-2    public int countSetBits(int n){
-3        int cnt = 0;
-4        while (n != 0) {
-5            n &= (n - 1);
-6            cnt++;
+2    public int[] countBits(int n) {
+3        int[] bits = new int[n + 1];
+4
+5        for (int i = 1; i <= n; i++) {
+6            bits[i] = bits[i & (i - 1)] + 1;
 7        }
-8        return cnt;
-9    }
-10    public int[] countBits(int n) {
-11        int[] arr = new int[n+1];
-12
-13        for(int i=0;i<arr.length;i++){
-14            arr[i] = countSetBits(i);
-15        }
-16
-17        return arr;
-18    }
-19}
+8
+9        return bits;
+10    }
+11}
