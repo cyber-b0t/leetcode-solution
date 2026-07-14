f0f4878d577f7f8bfc25d8f5e86b0731fd2521e7
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-07-14 18:04:18 +0530

    File updated at 7/14/2026, 6:04:18 PM

--- 190-ReverseBits/190-ReverseBits.java
+++ 190-ReverseBits/190-ReverseBits.java
@@ -1,13 +1,14 @@
-// Last updated: 7/14/2026, 6:02:07 PM
+// Last updated: 7/14/2026, 6:04:18 PM
 1class Solution {
 2    public int reverseBits(int n) {
 3        int res=0;
 4
-5        for(int i=0;i<32;i++){
-6            int bit = (n>>i)&1;
-7            res = res|(bit<<(31-i));
-8        }
-9
-10        return res;
-11    }
-12}
+5        for(int i=31;i>=0;i--){
+6            int bit = n&1;
+7            res = res + (bit<<i);
+8            n = n>>1;
+9        }
+10
+11        return res;
+12    }
+13}
