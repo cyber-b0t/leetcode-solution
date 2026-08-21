c19fedf9c714a898a5a4c6c73302a3fa2299653b
cyber-b0t <lakshitbargali23ec@rnsit.ac.in>
2026-08-22 02:30:22 +0530

    File updated at 8/22/2026, 2:30:22 AM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,24 +1,23 @@
-// Last updated: 6/24/2026, 8:53:56 PM
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-        int sum=0;
-        int count=0;
-        mpp.put(0,1);
-
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            int need = sum-k;
-
-            if(!mpp.containsKey(need)){
-                mpp.put(sum,mpp.getOrDefault(sum,0)+1);
-            }else{
-                count+=mpp.get(need);
-                mpp.put(sum,mpp.getOrDefault(sum,0)+1);
-            }
-
-        }
-
-        return count;
-    }
-}
+// Last updated: 8/22/2026, 2:30:22 AM
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        HashMap<Integer,Integer> mpp = new HashMap<>();
+4        int sum=0;
+5        int cnt=0;
+6        mpp.put(0,1);
+7        for(int i=0;i<nums.length;i++){
+8            sum+=nums[i];
+9            int need = sum-k;
+10            if(!mpp.containsKey(need)){
+11                mpp.put(sum,mpp.getOrDefault(sum,0)+1);
+12            }else{
+13                cnt+=mpp.get(need);
+14                mpp.put(sum,mpp.getOrDefault(sum,0)+1);
+15            }
+16            
+17            
+18        }
+19
+20        return cnt;
+21    }
+22}
